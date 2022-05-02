287cfe74e4553f60b7c001d31b61d32070e366ff
MalikSchinckus <73950955+MalikSchinckus@users.noreply.github.com>
2022-05-02 11:12:25 +0200

    Update product class

--- natif/src/main/java/Fonction_1/Products.java
+++ natif/src/main/java/Fonction_1/Products.java
@@ -1,5 +1,58 @@
-package Fonction_1;
-
 public class Products {
+	public Integer productId;
+	public String productName;
+	public String quantityPerUnit;
+	public Double unitPrice;
+	public Integer unitsInStock;
+	public Integer unitsOnOrder;
+	public Integer reorderLevel;
+	public Boolean discontinued;
+	
+	public Integer getProductId () {
+		return productId;
+	}
+	
+	public String getProductName () {
+		return productName;
+	}
+	public String getQuantity () {
+		return quantityPerUnit;
+	}
+	public Double getUnitPrice() {
+		return unitPrice;
+	}
+	public Integer getUnitsInStock() {
+		return unitsInStock;
+	}
+	public Integer getUnitsOnOrder () {
+		return unitsOnOrder;
+	}
+	public Integer getReorderLevel() {
+		return reorderLevel;
+	}
+	public Boolean getDiscontinued() {
+		return discontinued;
+	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
